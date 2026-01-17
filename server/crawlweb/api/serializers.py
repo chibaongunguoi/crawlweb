9460a1facba9dee0c8d751db52d8ff8ac38a3cab
chibaongunguoi <chibaongunguoi@gmail.com>
2026-01-17 21:35:56 +0700

    siuuuuu áp dụng thành công react + django bước đầu

--- server/crawlweb/api/serializers.py
+++ server/crawlweb/api/serializers.py
@@ -1,7 +1,19 @@
 from rest_framework import serializers
 from .models import JobDetail
+import json
 
 class JobDetailSerializer(serializers.ModelSerializer):
+    skills = serializers.SerializerMethodField()
+    
     class Meta:
         model = JobDetail
         fields = '__all__'
+    
+    def get_skills(self, obj):
+        """Convert skills to list if it's a string"""
+        if isinstance(obj.skills, str):
+            try:
+                return json.loads(obj.skills) if obj.skills else []
+            except (json.JSONDecodeError, ValueError):
+                return []
+        return obj.skills if obj.skills else []
--- server/crawlweb/crawlweb/settings.py
+++ server/crawlweb/crawlweb/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'api',
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -49,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'crawlweb.urls'
@@ -118,3 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+CORS_ALLOW_ALL_ORIGINS = True
