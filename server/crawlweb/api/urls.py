0767d6fa923e81230fd6232a0e2a8867e3e3f14a
chibaongunguoi <chibaongunguoi@gmail.com>
2026-01-17 21:04:21 +0700

    new

--- server/crawlweb/api/urls.py
+++ server/crawlweb/api/urls.py
@@ -3,6 +3,7 @@ from django.urls import include, path
 from .views import getJobDetail
 
 urlpatterns = [
+    # path("", include("rest_framework.urls")),
     path("jobs/", getJobDetail, name="recent_jobs"),
 
 ]
