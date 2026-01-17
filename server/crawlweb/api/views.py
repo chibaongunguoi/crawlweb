e1214c995e433c45ab57b0d9d01e689a174e2fba
chibaongunguoi <chibaongunguoi@gmail.com>
2026-01-17 19:49:40 +0700

    first commit

---
+++ server/crawlweb/api/views.py
@@ -0,0 +1,11 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+from .models import JobDetail
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the application index.")
+
+def recent_jobs(request):
+    jobs = JobDetail.objects.order_by("-collected_at")[:5]
+    return render(request, "recent_jobs.html", {"jobs": jobs})
